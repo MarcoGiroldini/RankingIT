6295795f3e13f4ff121ae9e82c78ad7ff929befa
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2017-08-22 11:21:23 +0200

    Calling api to register user on db when he logs in

--- client/src/app/Components/Home/home.component.ts
+++ client/src/app/Components/Home/home.component.ts
@@ -1,10 +1,11 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from "@angular/router";
 
 import { Observable } from "rxjs/Observable";
 import { AngularFireAuth } from "angularfire2/auth";
 import * as firebase from "firebase/app";
 import { AuthService } from "../../auth.service";
+import { HttpService } from "../../Services/http.service";
 
 
 @Component({
@@ -13,10 +14,24 @@ import { AuthService } from "../../auth.service";
     styleUrls: [ 'home.component.css']
 })
 export class HomeComponent {
+
     user: Observable<firebase.User> = null;
 
-    constructor(private authService: AuthService, private router: Router) {
-        this.user = authService.getAuthState();
+	constructor(private authService: AuthService, private router: Router, private httpService: HttpService) {
+		this.user = authService.getAuthState();
+
+		this.user.subscribe(userData => {
+			this.httpService.post(this.httpService.getApiServerUrl() + "api/login", {
+				// TODO: Object to send
+			}).subscribe(
+				data => {
+					console.log(data);
+				},
+				err => {
+					console.error(err);
+				}
+			);
+		})
     }
 
     ngOnInit() {
